e3a408b52033c611b33970591f4cfe29c98de5a9
Nicholas Avenell <nicholas@istic.net>
2026-06-27 17:18:23 +0100

    🎇 Share Matomo config as Inertia shared prop

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Services\MatomoService;
 use Illuminate\Http\Request;
 use Inertia\Middleware;
 
@@ -43,6 +44,7 @@ class HandleInertiaRequests extends Middleware
             ],
             'sidebarOpen' => ! $request->hasCookie('sidebar_state') || $request->cookie('sidebar_state') === 'true',
             'appVersion' => $this->resolveAppVersion(),
+            'matomo' => app(MatomoService::class)->getConfig(),
         ];
     }
 
